8d8a12fe78adc070f0bff36a0caf4442c7e53be0
Takatoshi Matsumoto <toqoz403@gmail.com>
2014-02-05 16:17:05 +0900

    Rough sketch

---
+++ main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+)
+
+type HTMLContent struct {
+	Content string
+}
+
+var (
+	layoutFilename = "_layout.html"
+	srcDirname     = "_src"
+)
+
+func main() {
+	httpFlag := flag.String("http", "", "addr. (e.g. \":8000\")")
+	flag.Parse()
+
+	var err error
+
+	if *httpFlag != "" {
+		err = serve(*httpFlag)
+	} else {
+		err = build()
+	}
+
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
+
+func serve(addr string) error {
+	wd, err := os.Getwd()
+
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Starging web server: %s", addr)
+	err = http.ListenAndServe(addr, http.FileServer(http.Dir(wd)))
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func build() error {
+	wd, err := os.Getwd()
+
+	if err != nil {
+		return err
+	}
+
+	srcPath := filepath.Join(wd, srcDirname)
+
+	if _, err := os.Stat(srcPath); err != nil {
+		return err
+	}
+
+	layoutFilepath := filepath.Join(srcPath, layoutFilename)
+
+	if _, err := os.Stat(layoutFilepath); err != nil {
+		return err
+	}
+
+	err = filepath.Walk(srcPath, func(inputFilepath string, fileInfo os.FileInfo, err error) error {
+		// skip directory
+		if fileInfo.IsDir() {
+			return nil
+		}
+
+		// skip layout file
+		if inputFilepath == layoutFilepath {
+			return nil
+		}
+
+		// create output file
+		outputFilepath := strings.Replace(inputFilepath, srcPath, wd, 1)
+
+		err = mkdirIfNotExist(filepath.Dir(outputFilepath))
+
+		if err != nil {
+			return err
+		}
+
+		out, err := os.Create(outputFilepath)
+
+		if err != nil {
+			return err
+		}
+
+		defer out.Close()
+
+		// open input file
+		in, err := os.Open(inputFilepath)
+
+		if err != nil {
+			return err
+		}
+
+		defer in.Close()
+
+		// build
+		switch filepath.Ext(in.Name()) {
+		case ".html":
+			content, err := ioutil.ReadFile(in.Name())
+
+			if err != nil {
+				return err
+			}
+
+			w := bufio.NewWriter(out)
+			t, err := template.ParseFiles(layoutFilepath)
+
+			if err != nil {
+				return err
+			}
+
+			t.Execute(w, &HTMLContent{string(content)})
+			w.Flush()
+		default:
+			_, err = io.Copy(out, in)
+
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+
+	return err
+}
+
+func mkdirIfNotExist(dir string) error {
+	_, err := os.Stat(dir)
+
+	if err != nil {
+		err := os.MkdirAll(dir, os.ModeDir|0755)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
